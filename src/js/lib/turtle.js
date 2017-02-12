af20656b7922734add97ac6b288e9ae2d020ec7f
Eirheinger <orban.valentin@gmail.com>
2017-02-12 18:30:39 +0100

    forgot to add turtle.js to commit, sorry

---
+++ src/js/lib/turtle.js
@@ -0,0 +1,80 @@
+const Vector = require('victor');
+
+/**
+ * Object representing the turtle seen on the canvas
+*/
+class Turtle {
+	/**
+	 * Instanciates a turtle
+	 * @param {Integer} x - position in the horizontal axis
+	 * @param {Integer} y - position in the vertical axis
+	 * @param {Float} angle - Direction. In degrees, angle between the direction's vector of Turtle and the X axis's vector
+	 * @param {Boolean} drawing - If true, movements will be drawn to canvas
+	 * @param {Boolean} hidden - If true, the turtle won't be shown on canvas, if false, it will be
+	 * @constructor
+	 */
+	constructor(x, y, angle, drawing, hidden) {
+		this.position = new Vector(x, y)
+		this.previousPos = new Vector(0,0);
+		this.angle = new Vector(1, 0);
+		this.angle.rotateDeg(angle).normalize();
+		this.drawing = drawing;
+		this.hidden = hidden;
+	}
+
+	draw() {
+		if (this.drawing) {
+			renderingContext.moveTo(this.previousPos.x, this.previousPos.y);
+			renderingContext.lineTo(this.position.x, this.position.y);
+			renderingContext.stroke();
+		}
+	}
+
+
+	/**
+	 * Move the turtle on a line defined by the turtle's direction (angle)
+	 * @param {Integer} distance - Distance to travel from current position to new position
+	*/
+	moveAlongDirection(distance) {
+		//TODO: move by a number of pixels, not by a distance
+		this.previousPos = this.position.clone();
+		this.position.add(
+			this.angle.clone().multiply(
+				new Vector(distance, distance)
+			)
+		);
+
+	}
+
+	/**
+	 * Set turtle's angle from the X axis
+	 * @param {Integer} angle - In degrees, the new angle
+	 */
+	setAngle(angle) {
+		this.angle.rotateDeg(angle).normalize();
+	}
+	/**
+	 * Add degrees to turtle's current angle
+	 * @param {Integer} degrees - Amount of degrees to add to current angle
+	 */
+	rotate(degrees) {
+		this.angle.rotateByDeg(degrees).normalize();
+	}
+
+	/**
+	 * Returns wether turtle is hidden or not
+	 * @return {Boolean}
+	*/
+	isHidden() {
+		return this.hidden;
+	}
+	/**
+	 * Returns wether movements will be drawn or not
+	 * @return {Boolean}
+	*/
+	isDrawing() {
+		return this.drawing;
+	}
+}
+
+module.exports = Turtle;
